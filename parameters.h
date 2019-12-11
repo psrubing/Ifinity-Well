35cf1d899b0da80707eb39a7c9cc0f225ff7b76f
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2019-12-11 21:39:51 +0100

    Update parameters.h

--- parameters.h
+++ parameters.h
@@ -8,19 +8,23 @@ struct Parameters{
 
     int N = 100;
     double delta_x = 1.0 /(double)N;
+    int S_0 = 400000;
+    int S_out = 1000;
+    const double Pi = 3.14159265359;
+    int n = 4;
+    double tau = 0.0;
+    double delta_tau = 0.0001;
+    int kappa = 10;
+    double omega = 3.0 * Pi * Pi / 2.0;
+
 
     //tablice
-    double psi_R[100+1];
-    double psi_l[100+1];
-
-    double ham_R[100+1];
-    double ham_l[100+1];
+    double psi_R[101];
+    double psi_l[101];
+    double ham_R[101];
+    double ham_l[101];
 
 
 };
-
-void count_ham_R(Parameters&, int, int, int);
-void count_ham_l(Parameters&, int, int, int);
-void count_psi(Parameters&, int, const double);
 
 #endif
